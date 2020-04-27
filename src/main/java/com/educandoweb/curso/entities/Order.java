9aa5f1e544055496829d4285fe26ffc6ecaee37f
diegosadu <diegosadu@gmail.com>
2020-04-27 10:47:44 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/curso/entities/Order.java
+++ src/main/java/com/educandoweb/curso/entities/Order.java
@@ -100,6 +100,14 @@ public class Order implements Serializable {
 		return items;
 	}
 	
+	public Double getTotal() {
+		double sum = 0;
+		for (OrderItem oi : items) {
+			sum += oi.getSubTotal();
+		}
+		return sum;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
